e84042ed1110c19471c1a2a9b2c2478776da2176
Shobitha Rani Pambala <shobitharanipambala@gmail.com>
2022-12-27 15:09:12 +0530

    shobitharani.github.io

--- counter.js
+++ counter.js
@@ -6,19 +6,25 @@ function updateCounter() {
      * if count is null then set count to 1
     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
     */
-  
+    // var count = 1;
+    // //console.log(count);
+    // if(count == null){
+    //    set.count(1);
+    // }
     if(count == null){
         count = 1;
-
-        
     }
-    else { 
-        count ++;
+    else{
+        count++;
     }
-
-
-    localStorage.setItem('visiterCount', count);
     
+    localStorage.setItem('visiterCount', count);
+
+   
+    // let count = localStorage.getItem('visiterLocation');
+    // localStorage.setItem('visiterLocation', location);
     counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
+    //counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterLocation')}`;
 }
 updateCounter();
+    
