c2fd71fac180f5f4e50a9935c64e79046000b76c
Shobitha Rani Pambala <shobitharanipambala@gmail.com>
2022-12-27 15:50:20 +0530

    shobitharani.github.io

--- counter.js
+++ counter.js
@@ -1,16 +1,7 @@
 function updateCounter() {
     const counter = document.getElementById('counter');
-    //increment the counter everytime user visits the page by storing in local storage
     let count = localStorage.getItem('visiterCount');
-    /**
-     * if count is null then set count to 1
-    * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
-    */
-    // var count = 1;
-    // //console.log(count);
-    // if(count == null){
-    //    set.count(1);
-    // }
+    
     
     if(count == null){
         count = 1;
